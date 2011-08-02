80cb77bb337cd338ccf3c3351d8e25b88d6e6ff9
tas0s <a.bourazanis@gmail.com>
2011-08-02 14:57:53 +0300

    remove unused classes/files

--- src/thesis/drmReader/util/IncrementalAsyncTaskLoaderCallbacks.java
+++
@@ -1,9 +0,0 @@
-package thesis.drmReader.util;
-
-import android.support.v4.app.LoaderManager;
-
-public interface IncrementalAsyncTaskLoaderCallbacks<T, V> extends LoaderManager.LoaderCallbacks<V> {
-public void onPreExecute();
-public void onProgressUpdate(T progress);
-public void onPostExecute(V result);
-}
